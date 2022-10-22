ded4bed49a29bdb9cd828ce30926ac5a2e15cdd2
Raebun <rayray14yt@gmail.com>
2022-10-22 16:01:20 +0200

    CustomerDTO and CustomerDAL added

---
+++ CustomerDAL.cs
@@ -0,0 +1,165 @@
+﻿using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+	public class CustomerDAL
+	{
+		//string connectionString = ConfigurationManager.ConnectionStrings['sql'].ConnectionString;
+		string connectionString;
+
+		public List<CustomerDTO> Read()
+		{
+			List<CustomerDTO> result = new List<CustomerDTO>();
+
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string sqlQuery = "select * from TABLE";
+					using (SqlCommand command = new SqlCommand(sqlQuery, con))
+					{
+						con.Open();
+						SqlDataReader reader = command.ExecuteReader();
+
+						while (reader.Read())
+						{
+							int customerId = reader.GetInt32(0);
+							string firstName = reader.GetString(0);
+							string lastName = reader.GetString(1);
+							string address = reader.GetString(2);
+							string city = reader.GetString(3);
+							string postalCode = reader.GetString(4);
+							string phone = reader.GetString(5);
+
+							CustomerDTO customer = new CustomerDTO();
+							customer.CustomerId = customerId;
+							customer.FirstName = firstName;
+							customer.LastName = lastName;
+							customer.Address = address;
+							customer.City = city;
+							customer.PostalCode = postalCode;
+							customer.Phone = phone;
+
+							result.Add(customer);
+
+						}
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+
+			}
+
+			return result;
+		}
+
+		public int Create(CustomerDTO customer)
+		{
+			int affectedRows = 0;
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string sqlQuery = "insert into TABLE (COLUMN, COLUMN) VALUES (@columnValues, @columnValues)";
+					using (SqlCommand command = new SqlCommand(sqlQuery, con))
+					{
+						command.Parameters.AddWithValue("@firstNameValues", customer.FirstName);
+						con.Open();
+
+						affectedRows = command.ExecuteNonQuery();
+
+						if (affectedRows > 0)
+						{
+							// created
+						}
+						else
+						{
+							// not created
+						}
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+
+			}
+
+			return affectedRows;
+		}
+
+		public int Delete(CustomerDTO customer)
+		{
+			int affectedRows = 0;
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string sqlQuery = "delete from TABLE where COLUMN = @columnValues";
+					using (SqlCommand command = new SqlCommand(sqlQuery, con))
+					{
+						command.Parameters.AddWithValue("@firstNameValues", customer.FirstName);
+						con.Open();
+
+						affectedRows = command.ExecuteNonQuery();
+
+						if (affectedRows > 0)
+						{
+							// deleted
+						}
+						else
+						{
+							// not deleted
+						}
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+
+			}
+
+			return affectedRows;
+		}
+
+		public int Update(CustomerDTO customer)
+		{
+			int affectedRows = 0;
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					string sqlQuery = "Update TABLE set COLUMN = @columnValues WHERE column = @columnValues";
+					using (SqlCommand command = new SqlCommand(sqlQuery, con))
+					{
+						command.Parameters.AddWithValue("@firstNameValues", customer.FirstName);
+						con.Open();
+
+						affectedRows = command.ExecuteNonQuery();
+
+						if (affectedRows > 0)
+						{
+							// updated
+						}
+						else
+						{
+							// not updated
+						}
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+
+			}
+
+			return affectedRows;
+		}
+	}
+}
---
+++ DTO/CustomerDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataAccess.DTO
+{
+	public class CustomerDTO
+	{
+		// Enum
+		enum CountryList
+		{
+			[Display(Name = "The Netherlands")] THE_NETHERLANDS,
+			[Display(Name = "Belgium")] BELGIUM
+		}
+
+		// Properties
+		private int customerId;
+		private string firstName;
+		private string lastName;
+		private string address;
+		private string city;
+		private string postalCode;
+		private CountryList country;
+		private string phone;
+
+		// Getters and setters
+		public int CustomerId { get { return customerId; } set { customerId = value; } }
+		public string FirstName { get { return firstName; } set { firstName = value; } }
+		public string LastName { get { return lastName; } set { lastName = value; } }
+		public string Address { get { return address; } set { address = value; } }
+		public string City { get { return city; } set { city = value; } }
+		public string PostalCode { get { return postalCode; } set { postalCode = value; } }
+		// country
+		public string Phone { get { return phone; } set { phone = value; } }
+	}
+}
